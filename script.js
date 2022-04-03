7d5988db6af09d0eb7ca906c8b8c31f885dcb225
pierrek6e <pierrecass75@gmail.com>
2022-04-04 00:44:22 +0700

    handle price and selected seats

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const space = "&nbsp;&nbsp;";
 const seatContainer = document.getElementById("seat-container");
+const totalPrice = document.getElementById("total-price");
+const seatsBooked = document.getElementById("seats-booked");
 
+let seatsBookedCount = 0;
 let seatRowArray = new Array(10);
 let seatsArray = [
   seatRowArray,
@@ -10,33 +13,54 @@ let seatsArray = [
   seatRowArray,
 ];
 
-// second prototype
-for (let i = 0; i < seatsArray.length; i++) {
-  for (let j = 0; j < seatRowArray.length; j++) {
-    if (j == 2 || j == 7) {
-      seatContainer.innerHTML += space;
-    } else {
-      let seat = document.createElement("button");
-      seat.setAttribute("id", i + "-" + j);
-      seat.classList.add("material-icons");
-      seat.classList.add("seat");
-      seat.innerHTML = "chair";
-      seatContainer.appendChild(seat);
-    }
-  }
-  seatContainer.innerHTML += "</br>";
-}
+init();
 
-//eventListener not added if done in above loop. DOM might not be ready;
-for (let i = 0; i < seatsArray.length; i++) {
-  for (let j = 0; j < seatRowArray.length; j++) {
-    if (j != 2 && j != 7) {
-      let seat = document.getElementById(i + "-" + j);
-      seat.addEventListener("click", toggleSeat);
+function init() {
+  for (let i = 0; i < seatsArray.length; i++) {
+    for (let j = 0; j < seatRowArray.length; j++) {
+      if (j == 2 || j == 7) {
+        seatContainer.innerHTML += space;
+      } else {
+        let seat = document.createElement("button");
+        seat.setAttribute("id", i + "-" + j);
+        seat.classList.add("material-icons");
+        seat.classList.add("seat");
+        seat.innerHTML = "chair";
+        seatContainer.appendChild(seat);
+      }
+    }
+    seatContainer.innerHTML += "</br>";
+  }
+
+  //eventListener not added if done in above loop. DOM might not be ready;
+  for (let i = 0; i < seatsArray.length; i++) {
+    for (let j = 0; j < seatRowArray.length; j++) {
+      if (j != 2 && j != 7) {
+        let seat = document.getElementById(i + "-" + j);
+        seat.addEventListener("click", toggleSeat);
+      }
     }
   }
 }
 
 function toggleSeat() {
-  this.classList.toggle("seat-booked");
+  if (this.classList.contains("seat-booked")) {
+    this.classList.remove("seat-booked");
+    seatsBookedCount--;
+  } else {
+    this.classList.add("seat-booked");
+    seatsBookedCount++;
+  }
+  adjustPrice();
+}
+
+function getTotalPrice() {
+  let movie = document.getElementById("movies");
+  let price = movie.value;
+  return price * seatsBookedCount;
+}
+
+function adjustPrice() {
+  totalPrice.innerHTML = getTotalPrice();
+  seatsBooked.innerHTML = seatsBookedCount;
 }
