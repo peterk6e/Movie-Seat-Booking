419a9b822ed18469579186ce600a80ffed949644
pierrek6e <pierrecass75@gmail.com>
2022-04-04 00:11:07 +0700

    try second way to manage seats

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const availableSeatHtml =
-  "<button class='material-icons seat'>chair</button>";
-const reservedSeatHtml = "xx<button class='material-icons'>chair</button>";
 const space = "&nbsp;&nbsp;";
-const searContainer = document.getElementById("seat-container");
+const seatContainer = document.getElementById("seat-container");
 
 let seatRowArray = new Array(10);
 let seatsArray = [
@@ -13,34 +10,33 @@ let seatsArray = [
   seatRowArray,
 ];
 
-class Seat {
-  constructor(row, col, htmlCode = availableSeatHtml, reserved = false) {
-    this.row = row;
-    this.col = col;
-    this.htmlCode = htmlCode;
-    this.reserved;
-  }
-
-  toggleSeat() {
-    console.log("ok");
-    if (!this.reserved) {
-      this.reserved = true;
-      this.htmlCode = reservedSeatHtml;
+// second prototype
+for (let i = 0; i < seatsArray.length; i++) {
+  for (let j = 0; j < seatRowArray.length; j++) {
+    if (j == 2 || j == 7) {
+      seatContainer.innerHTML += space;
     } else {
-      this.reserved = false;
-      this.htmlCode = availableSeatHtml;
+      let seat = document.createElement("button");
+      seat.setAttribute("id", i + "-" + j);
+      seat.classList.add("material-icons");
+      seat.classList.add("seat");
+      seat.innerHTML = "chair";
+      seatContainer.appendChild(seat);
+    }
+  }
+  seatContainer.innerHTML += "</br>";
+}
+
+//eventListener not added if done in above loop. DOM might not be ready;
+for (let i = 0; i < seatsArray.length; i++) {
+  for (let j = 0; j < seatRowArray.length; j++) {
+    if (j != 2 && j != 7) {
+      let seat = document.getElementById(i + "-" + j);
+      seat.addEventListener("click", toggleSeat);
     }
   }
 }
 
-// first prototype for theater
-for (let i = 0; i < seatsArray.length; i++) {
-  for (let j = 0; j < seatRowArray.length; j++) {
-    seatsArray[i][j] = new Seat(i, j, availableSeatHtml, false);
-    if (j == 2 || j == 7) {
-      seatsArray[i][j].htmlCode = space;
-    }
-    searContainer.innerHTML += seatsArray[i][j].htmlCode;
-  }
-  searContainer.innerHTML += "</br>";
+function toggleSeat() {
+  this.classList.toggle("seat-booked");
 }
